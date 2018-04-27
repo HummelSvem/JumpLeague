32d478effc32e5e679019309b5c9db2428f9bc36
HummelSvem <38613451+HummelSvem@users.noreply.github.com>
2018-04-27 17:32:42 +0200

    Create JLCheckSignsTask.php

---
+++ src/BluPlayz/JumpLeague/Tasks/JLCheckSignsTask.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Bluplayz\JumpLeague\Tasks;
+
+use Bluplayz\JumpLeague\Game\ArenaManager;
+use Bluplayz\JumpLeague\Main\JumpLeague;
+use pocketmine\scheduler\PluginTask;
+
+class JLCheckSignsTask extends PluginTask
+{
+
+    public function __construct(JumpLeague $owner)
+    {
+        parent::__construct($owner);
+    }
+
+    public function onRun($currentTick)
+    {
+        ArenaManager::checkSigns();
+    }
+
+}
